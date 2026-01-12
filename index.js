e95d70bd9d5cb875e2a6dd8ec03261b9313c7ab5
mudasir687abbas <mudasir687abbas@gmail.com>
2026-01-12 23:29:53 +0500

    Adding Cors

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import express from "express";
-
+import cors from "cors";
 const app = express();
 app.use(express.json());
-
+app.use(cors({
+  origin: "https://crude-mern-frontend.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
 app.get("/", (req, res) => {
   res.send("API running");
 });
