ee1b633a436c6caf4a01328c71fd218db81bc172
mudasir687abbas <mudasir687abbas@gmail.com>
2026-01-15 16:21:30 +0500

    Simple server to check connection with online database

--- index.js
+++ index.js
@@ -2,14 +2,30 @@ import express from "express";
 import cors from "cors";
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin: "https://crude-mern-frontend.vercel.app",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
-}));
+app.use(cors());
+
+import mysql from "mysql2/promise";
+let db;
+try {
+    db = await mysql.createConnection({
+      host: "sql12.freesqldatabase.com",
+      user: "sql12814377",
+      password: "lvNrIK6F93",
+      database: "sql12814377",
+      port: 3306,
+    });
+    console.log("Database connected");
+  } catch (error) {
+    console.log("Database connection failed",error.message);
+    db = null;
+  }
+
 app.get("/", (req, res) => {
   res.send("API running");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
 
 export default app;
